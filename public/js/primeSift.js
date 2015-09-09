57d7f5c83af847b57947ddf44eece434abd89962
ryan broughan & Jason Lane <student@epicodus.com>
2015-09-09 13:42:09 -0700

    Remove non prime from array of integers

--- public/js/primeSift.js
+++ public/js/primeSift.js
@@ -9,8 +9,19 @@ var primeSift = function() {
     return numArray;
   };
 
-  siftArray = function() {
+  siftArray = function(arr) {
+    arr.reverse();
+    for (var i = 2; i < arr.length; i++ ) {
 
+      for (var j = (i - 1); j < arr.length; j++) {
+        if (arr[j] % i === 0){
+          arr.splice(j, 1);
+          j -= 1;
+        }
+      }
+    }
+
+    return arr.reverse();
   };
 
   return { create : createArray,
--- public/js/tests.js
+++ public/js/tests.js
@@ -3,7 +3,21 @@ var expect = require('chai').expect;
 
 describe('primeSift#create', function() {
   var prime = primeSift();
+
   it('returns array of integers, input >= Z > 1', function() {
     expect(prime.create(3)).to.eql([3, 2]);
+  });
+});
+
+describe('primeSift#create', function() {
+  var prime2 = primeSift();
+  var prime = primeSift();
+  var arr = prime.create(10);
+  var arr2 = prime2.create(13);
+  it('removes all non-prime from the numArray', function() {
+    expect(prime.sift(arr)).to.eql([7, 5, 3, 2]);
+  });
+  it('removes all non-prime from the numArray', function() {
+    expect(prime.sift(arr2)).to.eql([13, 11, 7, 5, 3, 2]);
   });
 });
